092229e366b65ae318a2957eb88992b13dc3738c
chartchai21 <chartchai21@gmail.com>
2020-07-20 17:40:53 +0700

    test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   collectCoverage: true,
-  collectCoverageFrom: ['**/*.{vue}', '!**/node_modules/**', '!<rootDir>/dist/**',
-    '!<rootDir>/src/plugins/**', '!<rootDir>/tests/unit/**'
-  ],
   coverageReporters: ['lcov', 'text-summary']
 }
